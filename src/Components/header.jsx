5f12eeb768b2c275a65c7016aeea0cf88503d2f5
Sunil <s.sirvi1288@gmail.com>
2022-07-02 18:02:20 +0530

    updated responsive scss

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -18,12 +18,10 @@ export const Header = () => {
                         <li className="nav-item">
                             <a className="nav-link" href="#footer">Contact</a>
                         </li>
-                    </ul>
-                    {/* <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="https://sunil-1921.github.io/ui-color/">SignIn/Up</a>
+                            <a className="nav-link" href="https://github.com/Sunil-1921/ui-color/blob/master/CONTRIBUTING.md">Contribute</a>
                         </li>
-                    </ul> */}
+                    </ul>
                 </div>
             </nav>
         </>
