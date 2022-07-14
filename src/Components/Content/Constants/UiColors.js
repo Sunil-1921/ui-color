d1dd6bbe82a8fdb3a1a4ceeb4c080cf5e8247e63
Sunil <s.sirvi1288@gmail.com>
2022-07-14 19:52:55 +0530

    more colors added

--- src/Components/Content/Constants/UiColors.js
+++ src/Components/Content/Constants/UiColors.js
@@ -9,6 +9,10 @@ exports.UiColors = [
     },
     {
         title: 'Green Colors',
-        colors: ['#1FAA59', '#38CC77', '#02B290', '#50DBB4', '#3DBE29', '#4DD637', '#1C8D73', '#66AD47', '#6AC47E', '#6EC72D', '#22CB5C', '#00D84A']
+        colors: ['#1FAA59', '#38CC77', '#02B290', '#50DBB4', '#3DBE29', '#4DD637', '#66AD47', '#6AC47E', '#6EC72D', '#22CB5C', '#00D84A']
+    },
+    {
+        title: 'Yellow Colors',
+        colors: ['#E8BD0D', '#EDBF69', '#F4BE2C', '#DDD101', '#A77B06', '#D9D55B', '#F7CD2E']
     }
 ]
